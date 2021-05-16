1dad9e80e028c0d0c33d889543e607c8b02f9460
Alex Lai <alexlai@192-168-1-153.tpgi.com.au>
2021-05-16 19:13:55 +1000

    Created first playground wiht swift, testing with different datatypes

---
+++ Course 1/L1/FirstPlayground.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+// Hello World
+print("Hello World!")
+
+// Variable and Constant
+var greeting = "Hello Swift Developer"
+print(greeting)
+
+greeting = "Glad you are here"
+print(greeting)
+
+let myname = "Alex"
+print(myname)
+
+// Data Types
+var cityName: String = "Melbourne"
+var decimalNum: Double = 4.382913
+var wholeNumber: Int = 48
+var lightsOn: Bool = false
+
+print(cityName)
+print(decimalNum)
+print(wholeNumber)
+print(lightsOn)
+
