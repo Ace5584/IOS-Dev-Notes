de68aa889e247f2464f33126cbdafa567fc4845c
Alex Lai <alexlai@Alexs-MacBook-Air.local>
2021-05-17 13:14:51 +1000

    added code for array in playground file

--- Course 1/L2/Array.playground/Contents.swift
+++ Course 1/L2/Array.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var numList = [10, 11, 12, 13]
+
+print(numList.count)
+
+numList.append(69)
+
+print(numList)
+
+numList.removeLast()
+
+print(numList)
