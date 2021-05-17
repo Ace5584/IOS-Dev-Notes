06e62df86eae96f8c6c7a1a44fc560c01e1d35a3
Alex Lai <alexlai@192-168-1-153.tpgi.com.au>
2021-05-17 20:33:05 +1000

    Added code for dictionaries as playground

--- Course 1/L2/Dictionary.playground/Contents.swift
+++ Course 1/L2/Dictionary.playground/Contents.swift
@@ -1,3 +1,15 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var nameAge = ["David":28, "Tristan":23]
+
+if let davidAge = nameAge["David"]{
+    print(davidAge)
+    nameAge["David"] = 42
+}
+
+if let davidNewAge = nameAge["David"]{
+    print(davidNewAge)
+}
+
+nameAge.removeValue(forKey: "David")
+print(nameAge)
