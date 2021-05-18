ab5ece5197e6b746a5bce5689d447352562e64bc
Alex Lai <alexlai@Alexs-MacBook-Air.local>
2021-05-18 12:59:36 +1000

    Text Changer app

---
+++ Edx Swift Course/L3/TextChanger/TextChanger/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TextChanger
+//
+//  Created by Alex Lai on 18/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func textChange(_ sender: UIButton) {
+        print("You pressed this button! In @IBAction func textchange")
+    }
+    
+}
+
