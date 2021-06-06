dfb64bb68bd0be03582e641ea22a00a29ec68285
Ace5584 <aexace666@gmail.com>
2021-06-06 12:09:16 +1000

    Added Segue Project IOS app

---
+++ Edx Swift Course/L6/SegueProject/SegueProject/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SegueProject
+//
+//  Created by Alex Lai on 6/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindToHome(unwindSegue: UIStoryboardSegue){
+        
+    }
+
+}
+
